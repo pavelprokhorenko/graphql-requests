0aff6d482951bbd97d0d8b365d0e7778f0f32e75
Pavel <prohorenkopavel74@gmail.com>
2022-07-23 14:49:50 +0500

    remove useless `if` in graphql error

--- graphql_client/errors.py
+++ graphql_client/errors.py
@@ -3,9 +3,7 @@ class GraphQLError(Exception):
 
     def __init__(self, errors: list[dict]):
         message = "\n".join(
-            error.get("message") or "\n".join(error.get("messages"))
-            for error in errors
-            if error
+            error.get("message") or "\n".join(error.get("messages")) for error in errors
         )
 
         super().__init__(message)
