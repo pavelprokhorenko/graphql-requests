0b3f2ae95219f75b9d62aae734a5bc7d7df87bc6
Pavel <prohorenkopavel74@gmail.com>
2022-08-06 12:09:57 +0500

    fix variables assignment to graphql request

--- graphql_requests/errors.py
+++ graphql_requests/errors.py
@@ -1,9 +1,8 @@
 class GraphQLError(Exception):
     """Base exception for GraphQL clients."""
 
-    def __init__(self, errors: list[dict]):
+    def __init__(self, errors: list[dict]) -> None:
         message = "\n".join(
             error.get("message") or "\n".join(error.get("messages")) for error in errors
         )
-
         super().__init__(message)
--- graphql_requests/request.py
+++ graphql_requests/request.py
@@ -8,37 +8,50 @@ from graphql_requests.utils import (
 
 
 class GraphQLRequest:
-    """Build request data for outer GraphQL service."""
-
     def __init__(self, *, body: str, variables: dict[str, Any]) -> None:
+        """
+        Build request send data for outer GraphQL service
+        """
         self._body = body
-        self._variables = variables
+        self._variables = dict_keys_to_camel_case_recursively(variables)
 
     def set_variables(self, variables: dict[str, Any]) -> None:
-        """Override variables to send."""
+        """
+        Override variables to send
+        """
         camel_case_variables = dict_keys_to_camel_case_recursively(variables)
         self._variables = camel_case_variables
 
-    def extend_variables(self, variables: dict[str, Any]) -> None:
-        """Add new variables to existing ones."""
-        camel_case_variables = dict_keys_to_camel_case_recursively(variables)
+    def extend_variables(self, additional_variables: dict[str, Any]) -> None:
+        """
+        Add new variables to existing ones
+        """
+        camel_case_variables = dict_keys_to_camel_case_recursively(additional_variables)
         self._variables.update(camel_case_variables)
 
     def set_fragment(self, fragment: str) -> None:
-        """Add GraphQL fragment to request body."""
+        """
+        Add GraphQL fragment to request body
+        """
         clear_fragment = re.sub(r"[\n\r\s]{2,}", " ", fragment)
         self._body = "{} {}".format(self._body, clear_fragment)
 
-    def set_body_from_file(self, file_path: str, encoding: str = "UTF-8") -> None:
-        """Read scheme from file and override body to send."""
+    def set_scheme_from_file(self, file_path: str, *, encoding: str = "UTF-8") -> None:
+        """
+        Read scheme from file and set it to request body
+        """
         self._body = read_scheme_from_file(file_path, encoding=encoding)
 
     @property
     def body(self) -> str:
-        """Request scheme."""
+        """
+        Request scheme
+        """
         return self._body
 
     @property
     def variables(self) -> dict[str, Any]:
-        """Request payload."""
+        """
+        Request payload
+        """
         return self._variables
