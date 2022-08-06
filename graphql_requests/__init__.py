199a5f69fbc290c2f95ed69adb092108ed2dc3db
Pavel <prohorenkopavel74@gmail.com>
2022-08-06 14:18:46 +0500

    change package version

--- graphql_requests/__init__.py
+++ graphql_requests/__init__.py
@@ -1,6 +1,6 @@
 """GraphQL client, high performance, easy to use, sync/async support"""
 
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 
 from .client import GraphQLClient as GraphQLClient
 from .request import GraphQLRequest as GraphQLRequest
