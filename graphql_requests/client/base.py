fe4d3bd3febb605ddc6abeac6929183df381494c
Pavel <prohorenkopavel74@gmail.com>
2022-08-06 18:05:30 +0500

    add auto converting response data to snake case

--- graphql_requests/client/base.py
+++ graphql_requests/client/base.py
@@ -1,7 +1,8 @@
 import json
 from typing import Any, Dict, Union
 
-from graphql_requests.typedefs import JSONEncoder
+from graphql_requests.typedefs import JSONEncoder, SnakeCaseEncoder
+from graphql_requests.utils import to_snake_case
 
 
 class GraphQLBaseClient:
@@ -11,6 +12,7 @@ class GraphQLBaseClient:
         "_headers",
         "_cookies",
         "_json_serialize",
+        "_snake_case_serializer",
         "_timeout",
     ]
 
@@ -21,17 +23,21 @@ class GraphQLBaseClient:
         headers: Union[Dict[str, Any], None] = None,
         cookies: Union[Dict[str, Any], None] = None,
         json_serialize: Union[JSONEncoder, None] = json.dumps,
-        timeout: Union[float, None] = 15  # seconds
+        snake_case_serializer: Union[SnakeCaseEncoder, None] = to_snake_case,
+        timeout: Union[float, None] = 15,  # seconds
     ) -> None:
         if headers is None:
             headers = dict()
+        if cookies is None:
+            cookies = dict()
         headers["Content-Type"] = "application/json"
 
-        self._base_url = base_url
-        self._headers = headers
-        self._cookies = cookies
-        self._json_serialize = json_serialize
-        self._timeout = timeout
+        self._base_url: str = base_url
+        self._headers: Dict[str, Any] = headers
+        self._cookies: Dict[str, Any] = cookies
+        self._json_serialize: JSONEncoder = json_serialize
+        self._snake_case_serializer: SnakeCaseEncoder = snake_case_serializer
+        self._timeout: float = timeout
 
     def _build_send_data(
         self, query: str, operation_name: str, variables: Union[str, Dict[str, Any]]
@@ -58,6 +64,11 @@ class GraphQLBaseClient:
     def json_serialize(self) -> JSONEncoder:
         """Json serializer callable."""
         return self._json_serialize
+
+    @property
+    def snake_case_serializer(self) -> SnakeCaseEncoder:
+        """Snake case serializer callable."""
+        return self._snake_case_serializer
 
     @property
     def timeout(self) -> float:
